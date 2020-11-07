f21877ac4f4acd5958a9b7faac6ea0d6a81fd502
BepBop <33974452+BepBop@users.noreply.github.com>
2020-11-07 20:52:28 +0530

    Update python.py

--- python.py
+++ python.py
@@ -34,15 +34,15 @@ def myClick():
      "2": " and on a quest to save their village against drought" ,
      "3": " on an impossible mission of getting her daughter into a college",
      "4": " However on a mission of getting the dam built for their village" , 
-     "5": " with a buring desire to become a highly respectable memeber of the society",
+     "5": " with a burning desire to become a highly respectable member of the society",
      "6": " with a goal of becoming successful in life!",
-     "7": " with a deep desire of breakingfree from his monotonous job and finally travel the world!",
-     "8": " hoping to open his own bussiness one day"
+     "7": " with a deep desire to break free from his monotonous job and finally travel the world!",
+     "8": " hoping to open his own business one day"
            }
            
 
  if k==2 and len(e2.get())!=0:
-    myLabel4=Label(root,text="Once upon a time, " + random.choice(Setup) + " and " + e1.get() + Noun2[k2] + "magically summoned to the resuce!")
+    myLabel4=Label(root,text="Once upon a time, " + random.choice(Setup) + " and " + e1.get() + Noun2[k2] + "magically summoned to the rescue!")
     myLabel4.pack()
  
  if k==1 and len(e2.get())!=0:
@@ -61,17 +61,17 @@ def myClick():
  
  if k>0 and len(e2.get())!=0:
 
-    MonologueIntro=["Upon reaching the kingdom, ","Upon reaching the kingdom's court, ","Upon being summoned to the kingdom, ","Upong being summoned in the middle of dark winter night to the kingdom, ", "Upon being summoned to the kingdom's church ", "It was warm sunny morning when our hero appeared ","Warm sun shinning on the vast green grassland when our hero touched the soil of magic kingdom the first time"]
+    MonologueIntro=["Upon reaching the kingdom, ","Upon reaching the kingdom's court, ","Upon being summoned to the kingdom, ","Upon being summoned in the middle of dark winter night to the kingdom, ", "Upon being summoned to the kingdom's church ", "It was warm sunny morning when our hero appeared ","Warm sun shinning on the vast green grassland when our hero touched the soil of magic kingdom the first time"]
     
-    MonologueResponsible=["The King was thrilled.", "The King's Court was thrilled.","The King Kingdom was filled with up hope.","The king asked them to help them with fighting the dragon.","The King pleaded them to join them in conquest against the dragon.","The Kingdom was rejoyed with arrival of the new hero.", "Suddenly the whole kingdom was filled with joy for now they had a fighting chance"]
+    MonologueResponsible=["The King was thrilled.", "The King's Court was thrilled.","The King Kingdom was filled with up hope.","The king asked them to help them with fighting the dragon.","The King pleaded them to join them in conquest against the dragon.","The Kingdom was overjoyed with arrival of the new hero.", "Suddenly the whole kingdom was filled with joy for now they had a fighting chance"]
     
     MonologueHero=["To be the hero of the story,","The hero of our story,","Once a common mam now hero,","Our hero,"," Magic Kingdom last hero",]
     
-    MonologueFight=["went into the deep redwood forest","Our hero went deep to save the day","Charged the battle that was knocking at the kingdom's door","Our now hero listening to the problem found the courage and charged the battle!","Our hero went out to the battle on the Great Mount Volca","Our common man now hero lead the kingdom army to the warfield of King Doom","air was warm and night dark but hero feared none and charged the Dargon Mount"]
+    MonologueFight=["went into the deep redwood forest","Our hero went deep to save the day","Charged the battle that was knocking at the kingdom's door","Our now hero listening to the problem found the courage and charged the battle!","Our hero went out to the battle on the Great Mount Volca","Our common man now hero lead the kingdom army to the warfield of King Doom","air was warm and night dark but hero feared none and charged the Dragon Mount"]
     
-    MonologueEnd=["Hero passing through dense kingdom forest to the battlefield was suddenly attacked","Suddenly the hero was attacked by magic casters with fireballs","The oracks surrounded our hero","The dragon flew over our hero","Enemies hidden in thick forest surrounded our hero passing by to the battlefield","enemies arhers shot some arrows at our hero","Our hero party shot their warning shot at the dragon lair"]
+    MonologueEnd=["Hero passing through dense kingdom forest to the battlefield was suddenly attacked","Suddenly the hero was attacked by magic casters with fireballs","The oracks surrounded our hero","The dragon flew over our hero","Enemies hidden in thick forest surrounded our hero passing by to the battlefield","enemies archers shot some arrows at our hero","Our hero party shot their warning shot at the dragon lair"]
    
-    MonologueEnd1=["Our hero fougt bravely and was victorious","Our hero at the end saved this kingdom","Our hero fought the war and kindom was saved", "Everyone was happy when out hero returned from the conquest victorious!"]
+    MonologueEnd1=["Our hero fought bravely and was victorious","Our hero at the end saved this kingdom","Our hero fought the war and kingdom was saved", "Everyone was happy when out hero returned from the conquest victorious!"]
     myLabel5=Label(root,text=random.choice(MonologueIntro)+" "+random.choice(MonologueResponsible))
     myLabel5.pack()
     
@@ -121,3 +121,4 @@ myLabel3.pack(padx=(30), pady=(5))
 myButton.pack(padx=(30), pady=(20)) 
 
 root.mainloop()
+
